4053c2356faf2a0ac5a07797ea623bbca9f37231
sirmjune <junesamueljune@gmail.com>
2019-01-26 14:29:56 +0300

    add

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('index.html');
+	}
+    public function about()
+    {
+        $this->load->view('about.html');
+    }
+    public function ad_med()
+    {
+        $this->load->view('ad-med.html');
+    }
+    public function contact()
+    {
+        $this->load->view('contact.html');
+    }
+    public function health_centers()
+    {
+        $this->load->view('health_centers.html');
+    }
+    public function location1()
+    {
+        $this->load->view('location1.html');
+    }
+}
