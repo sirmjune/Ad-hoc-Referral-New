151687ac0f41d9feb1bacfe07de531d3aea07874
sirmjune <junesamueljune@gmail.com>
2019-02-06 15:35:03 +0300

    new

---
+++ application/views/ad-med.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <!--=============== basic  ===============-->
+        <meta charset="UTF-8">
+        <title>Ad-Med - Ad-hoc Referral </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="robots" content="index, follow"/>
+        <meta name="keywords" content=""/>
+        <meta name="description" content=""/>
+        <!--=============== css  ===============-->	
+        <link type="text/css" rel="stylesheet" href="/resources/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/resources/css/plugins.css">
+        <link type="text/css" rel="stylesheet" href="/resources/css/style.css">
+        <!--=============== favicons ===============-->
+        <link rel="shortcut icon" href="/resources/images/favicon.ico">
+    </head>
+    <body>
+        <div class="loader"><i class="fa fa-refresh fa-spin"></i></div>
+        <!--================= main start ================-->
+        <div id="main">
+            <!--=============== header ===============-->	
+            <header>
+                <div class="header-inner">
+                    <div class="logo-holder">
+                        <a href="/" class="ajax"><img src="/resources/images/logo.png" alt=""></a>
+                    </div>
+                    <div class="nav-button-holder">
+                        <div class="nav-button vis-m"><span></span><span></span><span></span></div>
+                    </div>
+                    <div class="nav-holder">
+                        <nav>
+                            <ul>
+                                <li><a href="/" class="ajax">Home</a></li>
+                                <li><a href="/ad_med" class="ajax pp act-link">Ad-Med</a></li>
+                                <li>
+                                    <a href="/about" class="ajax pa">About us </a>
+                                    <!-- Scroll navigation  -->
+                                    <ul>
+                                        <li><a href="/about#sec1" class="ajax custom-scroll-link">About us </a></li>
+                                        <li><a href="/about#sec3" class="ajax custom-scroll-link">Story </a></li>
+                                        <li><a href="/about#sec4" class="ajax custom-scroll-link">Services</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="/health_centers" class="ajax pp">Health Centers</a></li>
+                                <li><a href="/contact" class="ajax">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="share-container isShare"></div>
+                <a class="selectMe shareSelector transition">Social Media</a>
+            </header>
+            <!-- header end-->	
+            <!--=============== wrapper ===============-->	
+            <div id="wrapper">
+                <div class="content-holder elem scale-bg2 transition3" >
+                    <div class="content full-height mm">
+                        <div class="wrapper-inner full-height no-padding">
+					<!--=============== Change the value data-mwa="0" - at your in milliseconds. If you put a value of 0 - the function to turn off autoplay  ===============-->
+                        <div class="swiper-container" id="horizontal-slider" data-mwc="1" data-mwa="0">
+                                <div class="swiper-wrapper">
+                                    <!--=============== 1 ===============-->	
+                                    <div class="swiper-slide">
+                                        <br>
+                                        <div class="bg" style="background-image:url(/resources/images/bg/1.jpg)"></div>
+                                        <div class="overlay"></div>
+                                        <div class="zoomimage"><img src="/resources/images/bg/1.jpg" class="intense" alt=""><i class="fa fa-expand"></i></div>
+                                    </div>
+                                    <!--=============== 4 ===============-->	
+                                    <div class="swiper-slide">
+                                        <div class="bg" style="background-image:url(/resources/images/bg/1.jpg)"></div>
+                                        <div class="overlay"></div>
+                                        <div class="zoomimage"><img src="/resources/images/bg/1.jpg" class="intense" alt=""><i class="fa fa-expand"></i></div>
+                                    </div>
+                                    <!--=============== 2 ===============-->	
+                                    <div class="swiper-slide">
+                                        <div class="bg" style="background-image:url(/resources/images/bg/1.jpg)"></div>
+                                        <div class="overlay"></div>
+                                        <div class="zoomimage"><img src="/resources/images/bg/1.jpg" class="intense" alt=""><i class="fa fa-expand"></i></div>
+                                    </div>
+                                    <!--=============== 3 ===============-->	
+                                    <div class="swiper-slide">
+                                        <div class="bg" style="background-image:url(/resources/images/bg/1.jpg)"></div>
+                                        <div class="zoomimage"><img src="/resources/images/bg/1.jpg" class="intense" alt=""><i class="fa fa-expand"></i></div>
+                                        <div class="overlay"></div>
+                                    </div>
+                                </div>
+                                <div class="swiper-nav-holder hor">
+                                    <a class="swiper-nav arrow-left transition " href="#"><i class="fa fa-long-arrow-left"></i></a>
+                                    <a class="swiper-nav  arrow-right transition" href="#"><i class="fa fa-long-arrow-right"></i></a>
+                                </div>
+                            </div>
+                            <div class="pagination"></div>
+                        </div>
+                    </div>
+                    <div class="fixed-column">
+                        <div class="container">
+                            <section  class="no-border">
+                                <div class="section-title">
+                                    <h3>Ad-med App</h3>
+                                </div>
+                                <div class="project-details">
+									<h3>Ad Hoc Referral provides a convinient way of booking a medical professional at anytime through it mobile application.</h3>
+                                    <img src="/resources/images/downloads/google-play-badge.png" class="respimg" alt="">
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.  </p>
+                                    <ul class="descr">
+                                        <li><span>Date :</span> 26.05.2014 </li>
+                                        <li><span>Client :</span>  House Big </li>
+                                        <li><span>Status :</span> Completed </li>
+                                        <li><span>Location : </span>  <a href="https://goo.gl/maps/UzN5m" target="_blank"> Kharkiv Ukraine  </a></li>
+                                    </ul>
+                                </div>
+                                <div class="content-nav">
+                                    <ul>
+                                        <li><a href="#" class="ajax"><i class="fa fa-long-arrow-left"></i></a></li>
+                                        <li><span>/</span></li>
+                                        <li><a href="#" class="ajax"><i class="fa fa-long-arrow-right"></i></a></li>
+                                    </ul>
+                                    <div class="p-all">
+                                        <a href="/" class="ajax"><i class="fa fa-th-large"></i></a>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- wrapper end -->
+        </div>
+        <!-- Main end -->
+        <!--=============== google map ===============-->
+        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY" type="text/javascript"></script> 
+        <!--=============== scripts  ===============-->
+        <script type="text/javascript" src="/resources/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/resources/js/plugins.js"></script>
+        <script type="text/javascript" src="/resources/js/core.js"></script>
+        <script type="text/javascript" src="/resources/js/scripts.js"></script>
+    </body>
+</html>
---
+++ application/views/location.php
@@ -0,0 +1,247 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <!--=============== basic  ===============-->
+        <meta charset="UTF-8">
+        <title>Health Center - Location</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="robots" content="index, follow"/>
+        <meta name="keywords" content=""/>
+        <meta name="description" content=""/>
+        <!--=============== css  ===============-->	
+        <link type="text/css" rel="stylesheet" href="/resources/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/resources/css/plugins.css">
+        <link type="text/css" rel="stylesheet" href="/resources/css/style.css">
+        <!--=============== favicons ===============-->
+        <link rel="shortcut icon" href="/resources/images/favicon.ico">
+    </head>
+    <body>
+        <div class="loader"><i class="fa fa-refresh fa-spin"></i></div>
+        <!--================= main start ================-->
+        <div id="main">
+            <!--=============== header ===============-->	
+            <header>
+                <div class="header-inner">
+                    <div class="logo-holder">
+                        <a href="/" class="ajax"><img src="/resources/images/logo.png" alt=""></a>
+                    </div>
+                    <div class="nav-button-holder">
+                        <div class="nav-button vis-m"><span></span><span></span><span></span></div>
+                    </div>
+                    <div class="nav-holder">
+                        <nav>
+                            <ul>
+                                <li><a href="/" class="ajax">Home</a></li>
+                                <li><a href="/ad_med" class="ajax pp act-link">Ad-Med</a></li>
+                                <li>
+                                    <a href="/about" class="ajax pa">About us </a>
+                                    <!-- Scroll navigation  -->
+                                    <ul>
+                                        <li><a href="/about#sec1" class="ajax custom-scroll-link">About us </a></li>
+                                        <li><a href="/about#sec3" class="ajax custom-scroll-link">Story </a></li>
+                                        <li><a href="/about#sec4" class="ajax custom-scroll-link">Services</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="/health_centers" class="ajax">Health Centers</a></li>
+                                <li><a href="/contact" class="ajax">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="share-container isShare"></div>
+                <a class="selectMe shareSelector transition">Social Media</a>
+            </header>
+            <!--=============== wrapper ===============-->	
+            <div id="wrapper">
+                <div class="content-holder elem scale-bg2 transition3" >
+                    <div class="content">
+                        <!-- background animation  -->
+                        <div class="bg-animate"><img src="/resources/images/body-bg.png"  class="respimg" alt=""></div>
+                        <!-- wrapper inner -->	
+                        <div class="wrapper-inner">
+                            <section class="no-padding no-border">
+                                <!-- page title -->		
+                                <div class="container">
+                                    <div class="page-title no-border">
+                                        <h2><?php print_r($data->contact_id->full_name) ?></h2>
+                                        <h3><span>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true</span></h3>
+                                    </div>
+                                </div>
+                            </section>
+                            <div class="clearfix"></div>
+                            <div class="container">
+                                <section>
+                                    <div class="gallery-items three-coulms grid-small-pad popup-gallery">
+                                        <!-- 1 -->
+                                        <div class="row img-slide-con">
+                                            <div class="col-md-6 sli-one" style="padding: 15px">
+                                                <div class="card text-left">
+                                                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3989.7488701144043!2d32.5739425!3d0.3380637!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177dbb0f51509de1%3A0xea12334542674d8c!2sNew+Mulago+Hospital!5e0!3m2!1sen!2sug!4v1544510300764" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">New Mulago Hospital</h5>
+                                                        <p class="card-text">location</p>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <!-- 2 -->
+                                        <div class="gallery-item   interior industrial">
+                                            <div class="grid-item-holder">
+                                                <div class="box-item">
+                                                    <a href="/resources/images/folio/thumbs/1.jpg">
+                                                    <span class="overlay"></span> 
+                                                    <img  src="/resources/images/folio/thumbs/1.jpg"   alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- 3 -->
+                                        <div class="gallery-item   interior industrial">
+                                            <div class="grid-item-holder">
+                                                <div class="box-item">
+                                                    <a href="/resources/images/folio/thumbs/1.jpg">
+                                                    <span class="overlay"></span> 
+                                                    <img  src="/resources/images/folio/thumbs/1.jpg"   alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- 4 -->
+                                        <div class="gallery-item gallery-item-second  interior  industrial">
+                                            <div class="grid-item-holder">
+                                                <div class="box-item">
+                                                    <a href="/resources/images/folio/thumbs/1.jpg">
+                                                    <span class="overlay"></span> 
+                                                    <img  src="/resources/images/folio/thumbs/1.jpg"   alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- 5 -->
+                                        <div class="gallery-item  interior  industrial">
+                                            <div class="grid-item-holder">
+                                                <div class="box-item">
+                                                    <a href="/resources/images/folio/thumbs/4.jpg">
+                                                    <span class="overlay"></span> 
+                                                    <img  src="/resources/images/folio/thumbs/4.jpg"   alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="gallery-item  interior  industrial">
+                                            <div class="grid-item-holder">
+                                                <div class="box-item">
+                                                    <a href="/resources/images/folio/thumbs/1.jpg">
+                                                    <span class="overlay"></span> 
+                                                    <img  src="/resources/images/folio/thumbs/1.jpg"   alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- 6 -->
+                                        <div class="gallery-item   interior">
+                                            <div class="grid-item-holder">
+                                                <div class="box-item">
+                                                    <a href="/resources/images/folio/thumbs/1.jpg">
+                                                    <span class="overlay"></span> 
+                                                    <img  src="/resources/images/folio/thumbs/1.jpg"   alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- 7 -->
+                                        <div class="gallery-item   houses">
+                                            <div class="grid-item-holder">
+                                                <div class="box-item">
+                                                    <a href="/resources/images/folio/thumbs/1.jpg">
+                                                    <span class="overlay"></span> 
+                                                    <img  src="/resources/images/folio/thumbs/1.jpg"   alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- end gallery items -->	
+                                    <div class="row">
+                                        <div class="col-md-10">
+                                            <div class="project-details">
+												<h3><span>Internet tend to repeat predefined chunks as necessary, making this the first true</span></h3>
+                                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words</p>
+                                                <p>  All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words</p>
+                                                <ul class="descr">
+                                                    <li><span>Date :</span> 26.05.2014 </li>
+                                                    <li><span>Client :</span>  House Big </li>
+                                                    <li><span>Status :</span> Completed </li>
+                                                    <li><span>Location : </span>  <a href="https://goo.gl/maps/UzN5m" target="_blank"> Kharkiv Ukraine  </a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                                <!-- about text end -->
+
+                            </div>
+                        </div>
+                        <!-- wrapper inner end   -->
+                        <!-- parallax column   -->
+                        <div class="img-wrap">
+                            <div class="bg" style="background-image: url(/resources/images/bg/long/1.jpg)"  data-top-bottom="transform: translateY(300px);" data-bottom-top="transform: translateY(-300px);"></div>
+                        </div>
+                        <!-- parallax column end   -->
+                        <!--to top    -->
+                        <div class="to-top">
+                            <i class="fa fa-long-arrow-up"></i>
+                        </div>
+                        <!-- to top  end -->
+                        <!--=============== footer ===============-->
+                        <div class="height-emulator"></div>
+                        <footer>
+                            <div class="footer-inner">
+                                <div class="row">
+                                    <div class="col-md-5">
+                                        <a class="footer-logo ajax" href="index.html"><img src="/resources/images/footer-logo.png" alt=""></a>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="footer-adress">
+                                          <span>Tusky's Building Level 2,<br/>
+                                                Plot 1615, Kira Road,<br/>
+                                                Ntinda - Kampala, Central Region.<br/>
+                                                P.O.Box 2063 Kampala (U)</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <ul class="footer-contact">
+                                            <li>+256 (706) 414 199</li>
+                                            <li><a href="#">services@ad-hoc-referral.com</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-5"></div>
+                                    <div class="col-md-7">
+                                        <p> &#169; Ad-hoc Referral   2018.  All rights reserved.  </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="footer-decor"></span>
+                        </footer>
+                        <!-- footer end    -->
+                    </div>
+                    <!-- content  end  -->
+                </div>
+                <!-- content-holder  end  -->
+            </div>
+            <!-- wrapper end -->
+        </div>
+        <!-- Main end -->
+        <!--=============== google map ===============-->
+        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY" type="text/javascript"></script> 
+        <!--=============== scripts  ===============-->
+        <script type="text/javascript" src="/resources/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/resources/js/plugins.js"></script>
+        <script type="text/javascript" src="/resources/js/core.js"></script>
+        <script type="text/javascript" src="/resources/js/scripts.js"></script>
+    </body>
+</html>
